2666a8280592e5ccd3eaf3a279042795fad59b24
nick-pell <93624627+nick-pell@users.noreply.github.com>
2024-03-11 22:21:27 -0400

    Fixed discord bot and added finance testing

--- responses.py
+++ responses.py
@@ -1,8 +1,20 @@
+from random import choice, randint
+
 
 def get_response(user_input: str) -> str:
-    lowered = user_input.lower()
+    lowered: str = user_input.lower()
 
     if lowered == '':
-        return 'Silent'
+        return 'Well, you\'re awfully silent...'
     elif 'hello' in lowered:
-        return 'Hello there'
+        return 'Hello there!'
+    elif 'how are you' in lowered:
+        return 'Good, thanks!'
+    elif 'bye' in lowered:
+        return 'See you!'
+    elif 'roll dice' in lowered:
+        return f'You rolled: {randint(1, 6)}'
+    else:
+        return choice(['I do not understand...',
+                       'What are you talking about?',
+                       'Do you mind rephrasing that?'])
